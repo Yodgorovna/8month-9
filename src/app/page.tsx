c1070cda61234e0f70984f171ed83390597078f9
Yodgorovna <lobargoziyeva9@gmail.com>
2024-09-12 01:35:47 +0500

    fff

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,12 +4,5 @@ export default async function Home() {
   const data = await getServerSession(authOptions);
   console.log(data);
 
-  return (
-    <div>
-      Lorem ipsum dolor sit amet consectetur adipisicing elit. Aspernatur
-      officiis eum eaque obcaecati facilis earum cumque quaerat, assumenda,
-      veritatis asperiores cupiditate possimus sint omnis expedita modi aut fuga
-      nihil ab?
-    </div>
-  );
+  return <div></div>;
 }
